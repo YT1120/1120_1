17c3ffa30ef95316f0d98bfa95a5148014d42fc7
YT1120 <yingting941120@gmail.com>
2024-12-11 13:13:00 +0800

    1127/1204

---
+++ 1204.01.py
@@ -0,0 +1,11 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QLabel
+
+import sys
+from PyQt6.QtWidgets import QApplication, QLabel
+
+app = QApplication(sys.argv)
+window = QLabel("Hello, PyQt6!")
+window.resize(200, 150)
+window.show()
+sys.exit(app.exec())
